b19492dbf19cd723beef7132e294cc757475ab34
victorhenriq <victor_henriquesc@outlook.com>
2020-06-19 22:46:11 -0300

    atualização no arquivo index.js

--- www/js/index.js
+++ www/js/index.js
@@ -1,46 +1,38 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+alert ("app.checkConnection: function () {...");
+console.log ("app.checkConnection: function () {...");
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
+var networkState = navigator.connection.type;
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+var states = {};
+states[connection.UNKNOWN] = 'Unknown connection';
+states[connection.ETHERNET] = 'Ethernet connection';
+states[connection.CELL_2G] = 'Cell 2G connection';
+states[connection.CELL_3G] = 'Cell 3G connection';
+states[connection.CELL_4G] = 'Cell 4G connection';
+states[connection.CELL] = 'Cell generic connection';
+states[connection.NONE] = 'No network connection';
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+alert ('Connection type: ' + states[networkState]);
+console.log ('Connection type: ' + states[networkState]);
 
-        console.log('Received Event: ' + id);
-    }
+// Application Constructor
+initialize: function() {
+    document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+},
+
+// deviceready Event Handler
+//
+// Bind any cordova events here. Common events are:
+// 'pause', 'resume', etc.
+onDeviceReady: function() {
+    this.receivedEvent('deviceready');
+    app.checkConnection();
+},
+
+// Update DOM on a Received Event
+receivedEvent: function(id) {
+    
+}
 };
 
 app.initialize();
